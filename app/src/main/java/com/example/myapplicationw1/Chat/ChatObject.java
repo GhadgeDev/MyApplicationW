d506f23344224d00652b594a5ed3528e92c3a7b0
devenghadge13 <devenghadge13@gmail.com>
2020-09-17 08:15:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationw1/Chat/ChatObject.java
@@ -0,0 +1,31 @@
+package com.example.myapplicationw1.Chat;
+
+import com.example.myapplicationw1.User.UserObject;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ChatObject implements Serializable {
+    private String chatId;
+
+    private ArrayList<UserObject> userObjectArrayList = new ArrayList<>();
+
+    public ChatObject(String chatId){
+        this.chatId = chatId;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+    public ArrayList<UserObject> getUserObjectArrayList() {
+        return userObjectArrayList;
+    }
+
+
+
+
+    public void addUserToArrayList(UserObject mUser){
+        userObjectArrayList.add(mUser);
+    }
+}
